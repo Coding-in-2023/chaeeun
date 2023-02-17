ac70a456a6bcd3e51dabea29e2c14a4e9b773795
chaeeee <114077630+chaeeeeee@users.noreply.github.com>
2023-02-17 14:24:35 +0900

    다시해

---
+++ 1316.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();	//단어의 개수
+    	int count = 0;			//그룹단어 카운트
+    	
+    	for (int i = 0; i < N; i++) {
+    		String str = sc.next();
+    		
+    	}
+    	
+    }
+}
