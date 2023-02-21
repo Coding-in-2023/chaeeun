b3f183f70552fc285d755e47a9fcc519abc2624c
chaeeee <114077630+chaeeeeee@users.noreply.github.com>
2023-02-21 20:24:11 +0900

    알파벳 찾기

--- STEP5/10809.java
+++ STEP5/10809.java
@@ -8,7 +8,7 @@ public class Main {
     	
     	int alpha[] = new int[26]; 					//알파벳 길이의 배열
     	for (int i = 0; i < alpha.length; i++) {
-    		alpha[i] = -1;									//-1로 초기화
+    		alpha[i] = -1;									
     	}
     	
     	for (int i = 0; i < str.length(); i++) {	
