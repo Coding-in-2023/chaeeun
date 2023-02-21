83a97a65fb0efb9bdef82078b304e8afe3c6de78
chaeeee <114077630+chaeeeeee@users.noreply.github.com>
2023-02-21 20:32:58 +0900

    코딩은 체육과목 입니다

--- STEP3/25314.java
+++ STEP3/25314.java
@@ -5,7 +5,7 @@ public class Main {
     	
     	Scanner sc = new Scanner(System.in);
     	int N = sc.nextInt();
-    	int temp = N / 4;
+    	int temp = N / 4;   //입력받은 수를 4로 나눈 만큼 long 출
     	
     	for (int i = 0; i < temp; i++) {
     		System.out.print("long ");
